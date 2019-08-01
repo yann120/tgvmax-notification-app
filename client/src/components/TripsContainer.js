832329b00a95e6f1ce0dace620a273a7732ed2c7
Yann PETITJEAN <yann120@hotmail.fr>
2019-08-01 16:09:40 +0000

    new crud view with material ui is working

--- client/src/components/TripsContainer.js
+++ client/src/components/TripsContainer.js
@@ -1,115 +1,74 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { EditingState, DataTypeProvider } from '@devexpress/dx-react-grid';
-import {
-	Grid,
-	Table,
-	TableHeaderRow,
-	TableEditRow,
-	TableEditColumn,
-  } from '@devexpress/dx-react-grid-bootstrap4';
-import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
-
-
-const getRowId = row => row.id;
-
-const TableComponent = ({ ...restProps }) => (
-<Table.Table
-	{...restProps}
-	className="table-striped"
-/>
-);
-
-const DateFormatter = ({ value }) => value.replace(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})(.+$)/, '$3/$2/$1 $4:$5');
-
-const DateTypeProvider = props => (
-  <DataTypeProvider
-    formatterComponent={DateFormatter}
-    {...props}
-  />
-);
+import MaterialTable from 'material-table';
 
 class TripsContainer extends Component {
 	constructor(props) {
-		super(props)
+		super(props);
 		this.state = {
 			columns: [
-				{ name: 'departure_station', title: 'Departure' },
-				{ name: 'arrival_station', title: 'Arrival' },
-				{ name: 'from_date', title: 'From' },
-				{ name: 'to_date', title: 'To' },
+				{ title: "Departure", field: "departure_station" },
+				{ title: "Arrival", field: "arrival_station" },
+				{ title: "From", field: "from_date", type: 'datetime' },
+				{
+				  title: "To",
+				  field: "to_date",
+				  type: 'datetime' ,
+				},
 			  ],
-			rows: [],
-			dateColumns: ['from_date', 'to_date'],
+			  data: [],
 		}
-		this.commitChanges = this.commitChanges.bind(this);
-	};
-	
+	}
 	componentDidMount() {
 		axios.get('http://localhost:3001/api/v1/trips.json')
 			.then(response => {
 				console.log(response)
 				this.setState({
-					trips: response.data,
-					rows: response.data
+					data: response.data
 				})
+				
 			})
 			.catch(error => console.log(error))
 	};
 
-	commitChanges({ added, changed, deleted }) {
-		let { rows } = this.state;
-		if (added) {
-		  const startingAddedId = rows.length > 0 ? rows[rows.length - 1].id + 1 : 0;
-		  rows = [
-			...rows,
-			...added.map((row, index) => ({
-			  id: startingAddedId + index,
-			  ...row,
-			})),
-		  ];
-		}
-		if (changed) {
-		  rows = rows.map(row => (changed[row.id] ? { ...row, ...changed[row.id] } : row));
-		}
-		if (deleted) {
-		  const deletedSet = new Set(deleted);
-		  rows = rows.filter(row => !deletedSet.has(row.id));
-		}
-		this.setState({ rows });
-	  }
 
 	render() {
-		// var tripsInSearch = this.state.trips.filter(trip => trip.passed === false);
-		// var tripsPassed = this.state.trips.filter(trip => trip.passed === true);
-		const { rows, columns, dateColumns } = this.state;
-		return (
-
-			<div className="card">
-			  <Grid
-				rows={rows}
-				columns={columns}
-				getRowId={getRowId}
-			  >
-				<EditingState
-				  onCommitChanges={this.commitChanges}
-				/>
-				<DateTypeProvider
-            		for={dateColumns}
-          		/>
-				<Table
-            		tableComponent={TableComponent}
-          		/>
-				<TableHeaderRow />
-				<TableEditRow />
-				<TableEditColumn
-				  showAddCommand
-				  showEditCommand
-				  showDeleteCommand
-				/>
-			  </Grid>
-			</div>
-		);
+		return(
+		<MaterialTable
+      title="Trains"
+      columns={this.state.columns}
+      data={this.state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...this.state.data];
+              data.push(newData);
+              this.setState({ ...this.state, data });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...this.state.data];
+              data[data.indexOf(oldData)] = newData;
+              this.setState({ ...this.state, data });
+            }, 600);
+          }),
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...this.state.data];
+              data.splice(data.indexOf(oldData), 1);
+              this.setState({ ...this.state, data });
+            }, 600);
+          }),
+      }}
+    />
+	)
 	}
 }
 
